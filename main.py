e3440bdb66fbbbc32e098dec470f471f6a1d5267
HolyGasCan <51594166+holygascan@users.noreply.github.com>
2023-04-21 18:37:15 +0300

    Added bubble film calculation between two rings of different radius
    Signed-off-by: HolyGasCan <51594166+holygascan@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from screeninfo import get_monitors
 import copy
 
 in_to_mm = 25.4
-window_coeff = 0.8
+window_coeff = 0.7
 
 screen = [m for m in get_monitors() if m.is_primary][0]
 window_width = screen.width_mm / in_to_mm * window_coeff
 window_height = screen.height_mm / in_to_mm * window_coeff
 
-n = 100                 # amount of dots
-height = 4              # total height of cylinder
-r = 4                   # initial radius
+n = 50                  # amount of dots
+height = 7              # total height of cylinder
+r = 5                   # initial radius
+r2 = 8                  # radius of the second ring
 dh = height / n         # delta height
 dr = 0.001              # delta radius
 eps = 1 / (10 ** 10)    # error
@@ -21,15 +22,10 @@ bubble = [[],           # r
           []]           # height
 
 
-# theoretical curve
-min_theor = 3.39335
-x = np.linspace(0, 4)
-y = min_theor * np.cosh((x - height / 2) / min_theor)
-
-
 # initiating cylinder
+step = abs(r2 - r) / n
 for i in range(n + 1):
-    bubble[0].append(r)
+    bubble[0].append(r + step * i)
     bubble[1].append(i * dh)
 
 
@@ -39,11 +35,13 @@ def cyl_surface(r1, r2):
 
 
 # calc overall surface with changed radius
-def surface(func, index, r1):
+def surface(func, index, d):
     sur = 0
     cyl = copy.deepcopy(func)
-    cyl[0][index] = r1
-    cyl[0] = cyl[0][0:index] + [r1] * (len(cyl[0]) - 2 * index) + cyl[0][len(cyl[0])-index:len(cyl[0])]
+    cyl[0][index] = cyl[0][index] + d
+
+    cyl[0] = cyl[0][0:index] + [cyl[0][i] + d for i in range(index, (len(cyl[0]) - index))] + \
+             cyl[0][len(cyl[0])-index:len(cyl[0])]
 
     for i in range(len(cyl[0]) - 1):
         sur += cyl_surface(cyl[0][i], cyl[0][i + 1])
@@ -54,13 +52,13 @@ def surface(func, index, r1):
 # change r to minimize surface
 def minimize(func, index):
     r1 = func[0][index]
-    m_dr = surface(func, index, r1 - dr)
-    p_dr = surface(func, index, r1 + dr)
+    m_dr = surface(func, index, -dr)
+    p_dr = surface(func, index, dr)
 
     if abs(m_dr - p_dr) < eps or r1 - dr < 0:
         return "min"
 
-    current_surface = surface(func, index, r1)
+    current_surface = surface(func, index, 0)
 
     if m_dr < current_surface:
         return r1 - dr
@@ -79,8 +77,8 @@ if __name__ == "__main__":
     fig = plt.figure(figsize=(window_width, window_height))
     graph = fig.add_subplot(111)
     max_x = max(bubble[0][0], bubble[0][n])
-    graph.axis([-max_x - 1, max_x + 1, -1, height + 1])
-    graph.axvline(ymin=1/(height+2), ymax=1-1/(height+2), ls='--', color='cyan')
+    graph.axis([-max_x - 0.5, max_x + 0.5, -0.5, height + 0.5])
+    graph.axvline(ls='--', color='cyan')
 
     line_bot, = graph.plot([-bubble[0][0], bubble[0][0]], [0, 0],
                            color='blue')
@@ -90,8 +88,6 @@ if __name__ == "__main__":
                               color='blue')
     curve_left, = graph.plot([-x for x in bubble[0]], bubble[1],
                              color='blue')
-    cosh_right, = graph.plot(y, x,
-                             c='red')
 
     while "no" in minimized:
         for i in range(1, int(n / 2) + 1):
@@ -99,8 +95,8 @@ if __name__ == "__main__":
             if resp == "min":
                 minimized[i - 1] = "min"
             else:
+                bubble[0][n - i] = bubble[0][n - i] - (bubble[0][i] - resp)
                 bubble[0][i] = resp
-                bubble[0][n - i] = resp
                 minimized[i - 1] = "no"
         if min(bubble[0]) < 2 * eps:
             isPopped = True
@@ -125,6 +121,5 @@ if __name__ == "__main__":
     if not isPopped:
         graph.set_title("Finished!")
 
-    print(abs(min(bubble[0]) - min_theor))
     plt.ioff()
     plt.show()
